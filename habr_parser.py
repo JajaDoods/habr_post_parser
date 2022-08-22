e9d126bbbf1176892f1def325e32268753ca2d05
JajaDoods <bloorrare@proton.me>
2022-08-22 11:33:27 +0300

    Add user input

--- habr_parser.py
+++ habr_parser.py
@@ -13,29 +13,28 @@ from models import Article
 def main():
     console = Console()
 
-    # query = Prompt.ask('Enter query: ')
-    # pages = int(Prompt.ask('Pages: ', default='10'))
-    query = 'postgresql'
-    pages = 1
-    saving_file = f'{query}.json'
+    query = Prompt.ask('Enter query')
+    pages = int(Prompt.ask('Pages', default='10'))
+    saving_file = Prompt.ask('Save file', default=f'{query}_{pages}.json')
     articles = []
 
-    for page in range(1, pages+1):
-        response = requests.get(f'https://habr.com/ru/search/page{page}/?q={query}&target_type=posts&order=relevance')
-        if response.status_code != 200:
-            break
+    with console.status('Extract information') as status:
+        for page in range(1, pages+1):
+            response = requests.get(f'https://habr.com/ru/search/page{page}/?q={query}&target_type=posts&order=relevance')
+            if response.status_code != 200:
+                break
 
-        html = response.text
-        soup = BeautifulSoup(html, 'lxml')
+            html = response.text
+            soup = BeautifulSoup(html, 'lxml')
 
-        for article in soup.find_all('article'):
-            art_info = Article(article).get_info()
-            articles.append(art_info)
+            for ind, article in enumerate(soup.find_all('article')):
+                status.update(f'Extract {ind+1} article from {page} page.')
+                articles.append(Article(article).get_info())
         
-        time.sleep(.1)
+            time.sleep(.1)
 
     if not articles:
-        print('Something go wrong')
+        print('Something go wrong.')
         sys.exit(1)
 
     with open(saving_file, 'w', encoding='utf-8') as jsonfile:
