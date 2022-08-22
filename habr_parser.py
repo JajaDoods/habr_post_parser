0e7573b06f7b03fcdeabef43bda4c676b8fd5ae0
JajaDoods <bloorrare@proton.me>
2022-08-22 11:37:49 +0300

    Add user input

--- habr_parser.py
+++ habr_parser.py
@@ -16,9 +16,9 @@ def main():
     query = Prompt.ask('Enter query')
     pages = int(Prompt.ask('Pages', default='10'))
     saving_file = Prompt.ask('Save file', default=f'{query}_{pages}.json')
-    articles = []
 
     with console.status('Extract information') as status:
+        articles = []
         for page in range(1, pages+1):
             response = requests.get(f'https://habr.com/ru/search/page{page}/?q={query}&target_type=posts&order=relevance')
             if response.status_code != 200:
@@ -34,7 +34,7 @@ def main():
             time.sleep(.1)
 
     if not articles:
-        print('Something go wrong.')
+        console.print('Something go wrong.', style='red')
         sys.exit(1)
 
     with open(saving_file, 'w', encoding='utf-8') as jsonfile:
