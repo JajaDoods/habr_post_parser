91af93f9aadf65b273cdd334ae8772c0890eddb1
JajaDoods <bloorrare@proton.me>
2022-08-22 11:54:18 +0300

    Delete unusing module

--- models.py
+++ models.py
@@ -1,5 +1,4 @@
 import re
-from typing import Dict
 
 
 class Article:
@@ -12,14 +11,14 @@ class Article:
 
         self.root_link = 'https://habr.com'
 
-    def __publicatoin_date_info(self) -> None:
+    def __extract_publicatoin_date_info(self) -> None:
         '''
         Extract publication date
         '''
         self.publication_datetime = self.meta_container.find('time')['title'].replace(',', '')
 
 
-    def __author_info(self) -> None:
+    def __extract_author_info(self) -> None:
         '''
         Extract author information: username and link to account
         '''
@@ -28,7 +27,7 @@ class Article:
         self.author_name = user_info_block.text.strip()
         self.author_link = self.root_link + user_info_block['href']
         
-    def __article_info(self) -> None:
+    def __extract_article_info(self) -> None:
         '''
         Extract article information: article title and link to the article
         '''
@@ -37,13 +36,13 @@ class Article:
         self.article_link = self.root_link + header_container['href']
         self.article_title = header_container.span.text.strip()
 
-    def __views_info(self) -> None:
+    def __extract_views_info(self) -> None:
         '''
         Extract views of article
         '''
         self.num_views = self.counts_container.find('span', class_='tm-icon-counter__value').text.strip()
 
-    def __voices_info(self) -> None:
+    def __extract_voices_info(self) -> None:
         '''
         Extract information about voices on article 
         '''
@@ -63,26 +62,26 @@ class Article:
             'total_voices': total_voices
         }
 
-    def __bookmarks_info(self) -> None:
+    def __extract_bookmarks_info(self) -> None:
         '''
         Extract informatino about number of article bookmarks
         '''
         self.num_bookmarks = self.counts_container.find('span', class_='bookmarks-button__counter').text.strip()
 
-    def __comments_info(self) -> None:
+    def __extract_comments_info(self) -> None:
         '''
         Extract informatino about number of comments
         '''
         self.num_comments = self.counts_container.find('span', class_='tm-article-comments-counter-link__value').text.strip()
 
     def get_info(self) -> Dict[str, str]:
-        self.__publicatoin_date_info()
-        self.__author_info()
-        self.__article_info()
-        self.__views_info()
-        self.__voices_info()
-        self.__bookmarks_info()
-        self.__comments_info()
+        self.__extract_publicatoin_date_info()
+        self.__extract_author_info()
+        self.__extract_article_info()
+        self.__extract_views_info()
+        self.__extract_voices_info()
+        self.__extract_bookmarks_info()
+        self.__extract_comments_info()
 
         return {
             'publication_datetime': self.publication_datetime,
